416029524be2cc2b723b463d5bad63a86f363bb8
Anthony <anth06ny@gmail.com>
2022-11-29 16:07:57 +0100

    TP Thread1

--- app/src/main/java/com/amonteiro/a22_ynova/WeatherActivity.kt
+++ app/src/main/java/com/amonteiro/a22_ynova/WeatherActivity.kt
@@ -2,7 +2,9 @@ package com.amonteiro.a22_ynova
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.amonteiro.a22_ynova.databinding.ActivityWeatherBinding
+import kotlin.concurrent.thread
 
 class WeatherActivity : AppCompatActivity() {
 
@@ -13,5 +15,21 @@ class WeatherActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        binding.btLoad.setOnClickListener {
+
+            binding.progressBar.isVisible = true
+
+            thread {
+                //Appel on va chercher les donner
+                val weather = RequestUtils.loadWeather("Toulouse")
+
+                //Execution garantie sur l'UIThread
+                runOnUiThread {
+                    //Affichage des données
+                    binding.tvData.setText("Il fait ${weather.temperature.temp} °  à ${weather.name}")
+                    binding.progressBar.isVisible = false
+                }
+            }
+        }
     }
 }
