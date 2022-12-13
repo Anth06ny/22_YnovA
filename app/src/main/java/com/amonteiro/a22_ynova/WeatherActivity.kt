87ec328cf40e37b4bdcf899a2bccacd0f203df4a
Anthony <anth06ny@gmail.com>
2022-12-13 09:23:02 +0100

    Correction TP Exception

--- app/src/main/java/com/amonteiro/a22_ynova/WeatherActivity.kt
+++ app/src/main/java/com/amonteiro/a22_ynova/WeatherActivity.kt
@@ -12,38 +12,50 @@ class WeatherActivity : AppCompatActivity() {
     //Instancie le XML à retardement (by lazy) plus exactement à la 1er utilisation
     val binding by lazy { ActivityWeatherBinding.inflate(layoutInflater) }
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+
         binding.btLoad.setOnClickListener {
 
             binding.progressBar.isVisible = true
+            binding.tvError.isVisible = false
 
             thread {
-                //Appel on va chercher les donner
-                val weather = RequestUtils.loadWeather("Toulouse")
 
-                //Execution garantie sur l'UIThread
-                runOnUiThread {
-                    //Affichage des données
-                    binding.tv.text = weather.name
-                    binding.tvTemp.text = "${weather.temperature.temp}°"
-                    binding.tvWind.text = "${weather.wind.speed}"
-                    binding.tvMinMax.text = "(${weather.temperature.temp_min}°/${weather.temperature.temp_max}°)"
+                try {
+                    //Appel on va chercher les donner
+                    val weather = RequestUtils.loadWeather("Toulouse")
 
-                    if (weather.weather.isNotEmpty()) {
-                        binding.tvDesc.text = weather.weather[0].description
+                    //Execution garantie sur l'UIThread
+                    runOnUiThread {
+                        //Affichage des données
+                        binding.tv.text = weather.name
+                        binding.tvTemp.text = "${weather.temperature.temp}°"
+                        binding.tvWind.text = "${weather.wind.speed}"
+                        binding.tvMinMax.text = "(${weather.temperature.temp_min}°/${weather.temperature.temp_max}°)"
 
-                        Picasso.get().load("https://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png")
-                            .placeholder(R.mipmap.ic_launcher)
-                            .error(R.drawable.ic_baseline_delete_forever_24)
-                            .into(binding.ivTemp)
+                        if (weather.weather.isNotEmpty()) {
+                            binding.tvDesc.text = weather.weather[0].description
+
+                            Picasso.get().load("https://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png")
+                                .placeholder(R.mipmap.ic_launcher)
+                                .error(R.drawable.ic_baseline_delete_forever_24)
+                                .into(binding.ivTemp)
+                        }
+                        binding.progressBar.isVisible = false
                     }
+                }
+                catch (e: Exception) {
+                    e.printStackTrace()
 
-
-
-                    binding.progressBar.isVisible = false
+                    runOnUiThread {
+                        binding.tvError.text = "UNe erreur est survenue : ${e.message}"
+                        binding.tvError.isVisible = true
+                        binding.progressBar.isVisible = false
+                    }
                 }
             }
         }
